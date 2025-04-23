b342809bd9ac09ef73faf324c994e60c629c9400
SashaMagDEv <magnuh10@gmail.com>
2025-04-23 13:01:26 +0300

    corrected the task

--- homeworks/homework27/homework27_1/src/App.jsx
+++ homeworks/homework27/homework27_1/src/App.jsx
@@ -1,24 +1,19 @@
-import './App.css'
-import { Routes, Route } from 'react-router-dom';
-import Contacts from './pages/Contacts';
-import About from './pages/About';
-import  Home from './pages/Home.jsx'
-import Header from './components/Header';
-import ErrorBoundary from './components/ErrorBoundary';
+import React from 'react';
+import { Outlet, Link } from 'react-router-dom';
 
-function App() {
-
-  return (
-    <>
-        <Header />
-        <ErrorBoundary />
-        <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/contacts" element={<Contacts />} />
-            <Route path="/about" element={<About />} />
-        </Routes>
-    </>
-  )
+export default function App() {
+    return (
+        <div>
+            <header>
+                <nav>
+                    <Link to="/">Головна</Link> |
+                    <Link to="/about">Про мене</Link> |
+                    <Link to="/contacts">Контакти</Link>
+                </nav>
+            </header>
+            <main>
+                <Outlet />
+            </main>
+        </div>
+    );
 }
-
-export default App
